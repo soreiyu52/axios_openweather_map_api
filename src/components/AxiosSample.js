4f1bc35cfdf518117f5a0cbb3f019a15c75567c6
M.yusuke <soreiyu52@gmail.com>
2021-03-13 09:34:58 +0900

    axios sample4

--- src/components/AxiosSample.js
+++ src/components/AxiosSample.js
@@ -2,8 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import axiosJsonpAdapter from 'axios-jsonp'
 
-const config = {proxy: 'http://webservice.recruit.co.jp'}
-const API_ENDPOINT = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=9418d992ec5c342b&lat=34.67&lng=135.52&range=5&order=4?format=jsonp';
+const API_ENDPOINT = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=9418d992ec5c342b&lat=34.67&lng=135.52&range=5&order=4&format=jsonp';
 
 const AxiosSample = () => {
 
@@ -14,9 +13,7 @@ const AxiosSample = () => {
      */
     const getApi = () => {
         axios.get(API_ENDPOINT, {
-            'headers': {'Content-Type': 'application/xml'},
-            'responseType': 'xml',
-            'adapter': axiosJsonpAdapter
+            'adapter': axiosJsonpAdapter,
         }).then(res => {
             console.log(res)
         }).catch(error => {
